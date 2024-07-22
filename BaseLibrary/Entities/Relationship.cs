fa971589d043346047fbc98f034f588951752df9
Uallessonivo <uallessons@gmail.com>
2024-07-22 16:19:53 -0300

    feat: Add BaseEntity and related entity classes

---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+        // Relationships: One to Many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
+    }
+}
